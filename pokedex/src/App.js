d2ddfeee534fb61d4fac45a208123974ee6c59ce
Jonathan Chiang <jc91765@gmail.com>
2022-08-13 12:00:29 -0700

    tested fetch

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,9 +1,26 @@
-import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [pokemonData, setPokemonData] = useState(null);
+  const [isLoading, setLoad] = useState(true);
+
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon/1/')
+      .then(res => res.json())
+      .then(pokemon => setPokemonData(pokemon))
+      .then(setLoad(false));
+  }, []);
+
+  if (!isLoading) {
+    if (pokemonData) {
+      console.log(pokemonData.name, pokemonData.id);
+    }
+  }
+
   return (
     <div className="App">
       <h1>BLANK</h1>
+      <p></p>
     </div>
   );
 }
