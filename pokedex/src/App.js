b26fb3a3093f92a7d71be516770e0e7566337847
Jonathan Chiang <jc91765@gmail.com>
2022-08-22 21:28:30 -0700

    set up methods for searching

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -4,34 +4,33 @@ import DisplayPokemon from './components/display-pokemon';
 
 function App() {
   const [pokemonData, setPokemonData] = useState(null);
-  const [isLoading, setLoad] = useState(false);
+  const [loadMsg, setLoadMsg] = useState('');
   const [errorMsg, setErrorMsg] = useState('');
 
-  const fetchPokemon = async (name) => {
-    const query = `https://pokeapi.co/api/v2/pokemon/${name}/`
-    setLoad('fetching data...');
-    setErrorMsg('');
-    try {
-      const pokemonResponse = await fetch(query);
-      setLoad(false);
-      if (pokemonResponse.status === 404) return setErrorMsg(`'${name}' is an invalid search name!`);
-      return setPokemonData(await pokemonResponse.json());
-    } catch(err) {
-      console.log('error', err);
+  const fetchPokemon = async (name, param) => {
+    if (param === 'name') {
+      if (!name) return setErrorMsg('Please enter a valid name or id number!')
+      const query = `https://pokeapi.co/api/v2/pokemon/${name}/`
+      setLoadMsg('fetching pokemon data...');
+      setErrorMsg('');
+      try {
+        const pokemonResponse = await fetch(query);
+        setLoadMsg('');
+        if (pokemonResponse.status === 404) return setErrorMsg(`'${name}' is an invalid search name!`);
+        return setPokemonData(await pokemonResponse.json());
+      } catch (err) {
+        console.log('error', err);
+      }
+    } else {
+      console.log('type search method here');
     }
-  }
 
-  const loadMsg = () => {
-    if (isLoading) {
-      return <p>{'Fetching pokemon data...'}</p>
-    } else return;
   }
-
 
   return (
     <div className="App">
       <h1>POKEDEX SEARCH</h1>
-      {loadMsg()}
+      <p className="load-msg">{loadMsg}</p>
       <p className="error-msg">{errorMsg}</p>
       <SearchBar onSearch={fetchPokemon} />
       <DisplayPokemon pokemonInfo={pokemonData} />
--- pokedex/src/components/searchBar.jsx
+++ pokedex/src/components/searchBar.jsx
@@ -2,15 +2,20 @@ import { useState } from 'react';
 
 function SearchBar(props) {
   const [searchItem, setSearchItem] = useState('');
+  const [searchParam, setSearchParam] = useState('name');
 
   const handleChange = e => {
     setSearchItem(e.target.value);
   }
 
+  const changeSearchParam = e => {
+    setSearchParam(e.target.value);
+  }
+
   const handleSubmit = e => {
     e.preventDefault();
-    console.log('search:', searchItem);
-    props.onSearch(searchItem);
+    console.log();
+    props.onSearch(searchItem, searchParam);
     setSearchItem('');
   }
 
@@ -19,6 +24,10 @@ function SearchBar(props) {
       <form>
         <label htmlFor="name">Search for Pokemon:</label>
         <input name="pokemon" type="text" onChange={handleChange} value={searchItem}></input>
+        <select name="search-params" onChange={changeSearchParam}>
+          <option value="name">By Name or ID</option>
+          <option value="type">By Type</option>
+        </select>
         <button type="submit" onClick={handleSubmit}>SEARCH</button>
       </form>
     </div>
